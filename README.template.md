0910b7651dd29a74bf21ca2ccca5d42e63d1847e
Daher <daher.alfawares@live.com>
2020-11-28 02:37:57 -0600

    adding pagination

---
+++ README.template.md
@@ -0,0 +1,18 @@
+# name
+Welcome to name!
+## Installation
+To install the library simply run:
+```
+$ go get github.com/da0x/golang/name
+```
+To import the library, use:
+```
+import "github.com/da0x/golang/name"
+```
+## Usage
+Example:
+```
+code...
+```
+## Maintainer
+Your Name
---
+++ pagination/pagesize.go
@@ -0,0 +1,48 @@
+//
+//   pagination/pagesize.go
+//   golang
+//
+//   Copyright 2020 Daher Alfawares
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License
+//   You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2
+//
+//   Unless required by applicable law or agreed to in writing,
+//   distributed under the License is distributed on an "AS IS" BASIS
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied
+//   See the License for the specific language governing permissions
+//   limitations under the License
+
+package pagination
+
+import (
+	"log"
+	"net/url"
+	"strconv"
+)
+
+type PageSize struct {
+	Page, Size int
+}
+
+func (ol *PageSize) Next() {
+	ol.Page += 1
+}
+
+func (ol *PageSize) Values() url.Values {
+	v := url.Values{}
+	v.Add("size", strconv.Itoa(ol.Size))
+	v.Add("page", strconv.Itoa(ol.Page))
+	return v
+}
+
+func NewPageSize(size string) PageSize {
+	s, err := strconv.Atoi(size)
+	if err != nil {
+		log.Fatalf("pagination: error converting page size: %v", err)
+	}
+	return PageSize{0, s}
+}
