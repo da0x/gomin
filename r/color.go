f2667ba7cebb7669ede64784eefcd1a206f582de
Daher Alfawares <dalfawares@tesla.com>
2023-12-18 14:07:42 -0800

    updating to use faith/color

--- r/color.go
+++ r/color.go
@@ -1,21 +1,22 @@
 package r
 
-var End = "\033[0m"
-var Red = "\033[31m"
-var Green = "\033[32m"
-var Yellow = "\033[33m"
-var Blue = "\033[34m"
-var Purple = "\033[35m"
-var Cyan = "\033[36m"
-var Gray = "\033[37m"
-var White = "\033[97m"
+import (
+	"github.com/fatih/color"
+)
 
-var E = "\033[0m"
-var R = "\033[31m"
-var G = "\033[32m"
-var Y = "\033[33m"
-var B = "\033[34m"
-var P = "\033[35m"
-var C = "\033[36m"
-var A = "\033[37m"
-var W = "\033[97m"
+var Red = color.New(color.FgRed).SprintFunc()
+var Green = color.New(color.FgGreen).SprintFunc()
+var Yellow = color.New(color.FgYellow).SprintFunc()
+var Blue = color.New(color.FgBlue).SprintFunc()
+var Purple = color.New(color.FgMagenta).SprintFunc()
+var Cyan = color.New(color.FgCyan).SprintFunc()
+var Gray = color.New(color.FgWhite).SprintFunc() // FgWhite is often used as gray in terminal color schemes
+var White = color.New(color.FgHiWhite).SprintFunc()
+
+var BrightRed = color.New(color.FgHiRed).SprintFunc()
+var BrightGreen = color.New(color.FgHiGreen).SprintFunc()
+var BrightYellow = color.New(color.FgHiYellow).SprintFunc()
+var BrightBlue = color.New(color.FgHiBlue).SprintFunc()
+var BrightPurple = color.New(color.FgHiMagenta).SprintFunc()
+var BrightCyan = color.New(color.FgHiCyan).SprintFunc()
+var BrightGray = color.New(color.FgHiBlack).SprintFunc() // FgHiBlack is often used as bright gray in terminal color schemes
